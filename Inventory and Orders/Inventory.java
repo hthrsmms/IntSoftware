69320354011bb11b2cb4b68aa6200c14cc9a70cf
hthrsmms <simm5792@stthomas.edu>
2016-02-26 20:13:57 -0600

    Inventory & Orders

---
+++ Inventory and Orders/Inventory.java
@@ -0,0 +1,81 @@
+
+public class Inventory {
+	String number;
+	String name;
+	int quantity; // quantity of item in stock
+	double price;
+	String supplier;
+	int threshold;
+
+	// Set up inventory with beginning quantity in stock
+	public Inventory(String productNumber, String productName, int productQuantity, double productPrice,
+			String productSupplier, int productThreshold) {
+		number = productNumber;
+		name = productName;
+		quantity = productQuantity;
+		price = productPrice;
+		supplier = productSupplier;
+		threshold = productThreshold;
+	}
+
+	// Adds items to inventory
+	public int increaseInventory(int amount) {
+		if (amount > 0)
+			quantity = quantity + amount;
+		return quantity;
+	}
+
+	// Decrease inventory by amount of outstanding order
+	public double decreaseInventory(int amount) {
+		if (amount > 0)
+			quantity = quantity - amount;
+		return quantity;
+	}
+
+	// Current quantity of inventory
+	public int getQuantity() {
+		return quantity;
+	}
+
+	// Calculate price of inventory
+	public double getInventoryPrice() {
+		return price * quantity;
+	}
+
+	// Calculate how far from reorder
+	public int getInventoryThreshold() {
+		return quantity - threshold;
+	}
+
+	// if less than 5 units in stock, set status to reorder
+	public String getThresholdStatus() {
+		int threshCalculated;
+		String threshOrder;
+		threshCalculated = quantity - threshold;
+
+		if (threshCalculated > 5)
+			threshOrder = "Plenty in stock";
+		else
+			threshOrder = "REORDER SOON!";
+		return threshOrder;
+	}
+
+	// calculates total price of inventory
+	/*
+	 * public static double getTotalPriceOfInventory(Inventory[] inv){
+	 * 
+	 * double totalPrice = 0.0;
+	 * 
+	 * for (int i = 0; i < inv.length; i++) { totalPrice +=
+	 * inv[i].getInventoryPrice(); } return totalPrice; }
+	 */
+
+	// Inventory information
+	public String toString() {
+		return "Product: " + name + "\nProduct Number: " + number + "\nSupplier: " + supplier + "\nPrice Per Item: $"
+				+ price + "\nTotal Price in Inventory: $" + getInventoryPrice() + "\nQuantity in Stock: " + quantity
+				+ "\nReorder Threshold: " + threshold + "\nHow Close to Threshold: " + getInventoryThreshold()
+				+ "\nReorder Status: " + getThresholdStatus();
+	}
+
+}
---
+++ Inventory and Orders/Orders.java
@@ -0,0 +1,24 @@
+
+public class Orders {
+
+	// Creates inventory and orders
+
+	public static void main(String[] args) {
+		Inventory product1 = new Inventory("ABC100", "Apples", 10, 1.00, "Supplier1", 1);
+		Inventory product2 = new Inventory("DEF200", "Oranges", 20, 2.00, "Supplier2", 10);
+		Inventory product3 = new Inventory("XYZ300", "Bananas", 15, .55, "Supplier3", 15);
+
+		// testing increasing and decreasing inventory
+		product1.increaseInventory(20);
+		product2.decreaseInventory(10);
+
+		// print current inventory info
+		System.out.println();
+		System.out.println(product1);
+		System.out.println();
+		System.out.println(product2);
+		System.out.println();
+		System.out.println(product3);
+
+	}
+}
